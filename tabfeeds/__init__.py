89ad210a81a16f6c04e27edcfe7d682dc1218d2f
Jonathan Camile <jonathan.camile+github@gmail.com>
2016-10-27 01:39:55 +0200

    Process data for order webhook

--- tabfeeds/__init__.py
+++ tabfeeds/__init__.py
@@ -53,7 +53,7 @@ def products():
     return Response(xml, mimetype='text/xml')
 
 
-@app.route('/order_hook')
+@app.route('/order_hook', methods=['POST'])
 def order_hook():
     try:
         request.headers['X-Shopify-Topic']
@@ -65,4 +65,12 @@ def order_hook():
     if not _hmac_is_valid(request.get_data(), '4b543c40d4156b4a97948f4aea67ede2'.encode(), webhook_hmac):
         current_app.logger.error('HMAC not valid for webhook')
         return abort(403)
+    data = request.get_json()
+    customer_email = data['customer']['email']
+    customer_lang = None
+    for attr in data['note_attributes']:
+        if attr['name'] == 'language':
+            customer_lang = attr['value']
+    current_app.logger.info(customer_email)
+    current_app.logger.info(customer_lang)
     return 'ok'
